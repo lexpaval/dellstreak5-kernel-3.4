521cd5991542a6e134ebeb5733455e91c3f75706
lexu <lexu@lexu-Inspiron-N5110.(none)>
2012-12-30 20:57:39 +0200

    Initial commit Cloned from CAF MSM 3.4 kernel. Added AOSP 4.6.3 and Linaro 4.7.3 toolchains. Added dsc_defconfig. Added board-qsd8x50_austin + dependencies. Fixed 2 or 3 compile errors, can't remember where but one was in domain.h.
    TODO: Fix Compile Errors

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Sun Dec 30 09:19:57 EET 2012"
+#define LINUX_COMPILE_BY "lexu"
+#define LINUX_COMPILE_HOST "lexu-Inspiron-N5110"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-LTX-Kernel-v0.1"
